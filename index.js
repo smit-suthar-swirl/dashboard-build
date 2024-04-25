602b04a648916f0639345fe20fdb9d21bbbd4d52
Smit Suthar <smit@goswirl.live>
2024-04-25 15:24:34 +0530

    build pushed to engagement tab

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
