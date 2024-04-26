4795d744d428ad4fc3cc76766b94db9c8387e046
Smit Suthar <smit@goswirl.live>
2024-04-26 12:36:54 +0530

    compression added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
-
+const compression = require('compression');
 const app = express();
+
+app.use(compression());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
