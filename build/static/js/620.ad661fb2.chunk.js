e75dae6e2322d19b85b8472929a268e53adc6d3d
Smit Suthar <smit@goswirl.live>
2024-06-02 14:59:20 +0530

    date filter forwording and table responsive

---
+++ build/static/js/620.ad661fb2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkswirl_dashboard=self.webpackChunkswirl_dashboard||[]).push([[620],{32620:(L,t,a)=>{a.d(t,{E_0:()=>s});var r=a(13441);function s(L){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M4,5 L20,5 L20,23 L4,23 L4,5 Z M1,5 L23,5 M9,1 L15,1 L15,5 L9,5 L9,1 Z M9,1 L15,1 L15,5 L9,5 L9,1 Z M15,9 L15,19 M9,9 L9,19"},child:[]}]})(L)}}}]);
+//# sourceMappingURL=620.ad661fb2.chunk.js.map
